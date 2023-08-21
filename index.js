5a63263888540813e29cf1f67ce382b41a01a338
NikaN <NikaN7602@gmail.com>
2023-08-21 14:35:22 +0200

    index.js

--- index.js
+++ index.js
@@ -7,15 +7,15 @@
 // Собираем все звезды в псевдоколлекцию, используя для этого необходимый метод
 // Используя цикл, на каждую звезду добавляем обработчик события для изменения ее цвета
 // Пишем логику обработчика события
-let collectionStars = document.querySelectorAll(’.star’);
+let collectionStars = document.querySelectorAll('.star');
 console.log(collectionStars);
-for(let i = 0; i < collectionStars.length; i++) {
-collectionStars[i].addEventListener(‘click’, () => {
-for(let j = 0; j < i; j++) {
-collectionStars[i].classList.add(’.star_active’);
-}
-for(let j = i + 1; j < collectionStars.length; j++) {
-collectionStars[i].classList.remove(’.star_active’);
-}
-});
+for (let i = 0; i < collectionStars.length; i++) {
+    collectionStars[i].addEventListener('click', () => {
+        for (let j = 0; j < i; j++) {
+            collectionStars[i].classList.add('.star_active');
+        }
+        for (let j = i + 1; j < collectionStars.length; j++) {
+            collectionStars[i].classList.remove('.star_active');
+        }
+    });
 }
